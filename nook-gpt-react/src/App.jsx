2fe99813f9f3f8c838df91c3bcf5f56bfd82c50b
oprah-cares <emilykleinstl@gmail.com>
2024-04-08 18:59:53 -0500

    Set up some routing and navigation bar.

--- nook-gpt-react/src/App.jsx
+++ nook-gpt-react/src/App.jsx
@@ -1,11 +1,19 @@
 import './App.css'
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import NavBar from './components/NavBar';
+import Home from './components/Home';
+import Account from './components/Account';
 
-function App() {
 
+const App = () => {
   return (
-    <>
-      
-    </>
+    <Router>
+    <div>
+    <NavBar />
+    
+    </div>
+    </Router>
   )
 }
 
